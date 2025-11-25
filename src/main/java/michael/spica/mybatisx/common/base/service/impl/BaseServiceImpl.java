0a8e6039d34fd3f0bfeba7969bf080d00a100355
michael <michael.7520@qq.com>
2025-11-25 17:00:21 +0800

    fix: 多个 Mapper 同时存在时，启动报错

--- src/main/java/michael/spica/mybatisx/common/base/service/impl/BaseServiceImpl.java
+++ src/main/java/michael/spica/mybatisx/common/base/service/impl/BaseServiceImpl.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import lombok.extern.slf4j.Slf4j;
 import michael.spica.mybatisx.common.base.service.BaseService;
 import michael.spica.mybatisx.common.base.service.support.BaseServiceImplSupport;
-import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
 
@@ -12,7 +11,6 @@ import java.io.Serializable;
  * Created by michael on 2025-11-25.
  */
 @Slf4j
-@Service
 public class BaseServiceImpl<M extends BaseMapper<T>, T, ID extends Serializable>
         extends BaseServiceImplSupport<M, T, ID>
         implements BaseService<T, ID> {
